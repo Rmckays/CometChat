8d5848ea98577d9809c46440438d57fe30a7e4f6
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-08-26 20:27:18 -0700

    Clean up login.jsx form to use jsx class.

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const loginForm = () => {
    return(
-      <div class="showcase">
+      <div className="showcase">
          <form id="login-form" class="col-md-2 col-sm-4">
-            <h2 class="text-white text-center">Welcome to CometChat</h2>
+            <h2 className="text-white text-center">Welcome to CometChat</h2>
             <div id="form-group">
-               <label for="username" class="text-white">Username:</label>
-               <input type="text" class="form-control" />
+               <label for="username" className="text-white">Username:</label>
+               <input type="text" className="form-control" />
             </div>
             <div class="form-group">
-               <label for="password" class="text-white">Password:</label>
-               <input type="password" class="form-control" />
+               <label for="password" classNave="text-white">Password:</label>
+               <input type="password" className="form-control" />
             </div>
-            <div class="form-group">
-               <button id="login-btn" type="button" class="btn form-control">
+            <div className="form-group">
+               <button id="login-btn" type="button" className="btn form-control">
                Login
                </button>
             </div>
@@ -22,3 +22,5 @@ const loginForm = () => {
       </div>
    )
 }
+
+export default loginForm;
