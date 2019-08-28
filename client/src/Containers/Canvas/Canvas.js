7ddba3a4c167cc3e82e6eaf3b08989008b273f6f
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-08-28 14:06:40 -0700

    Refactor drawCanvas and drawGradient methods

--- client/src/Containers/Canvas/Canvas.js
+++ client/src/Containers/Canvas/Canvas.js
@@ -12,10 +12,16 @@ class Canvas extends Component {
    }
 
    componentDidMount() {
-      const canvas = this.canvasRef.current;
-      canvas.width = this.state.width;
-      canvas.height = this.state.height;
-      const context = canvas.getContext('2d');
+      this.drawCanvas();
+   }
+
+   componentDidUpdate() {
+      this.setState({ height: window.innerHeight });
+      this.setState({ width: window.innerWidth });
+      this.drawCanvas();
+   }
+
+   drawGradient(context) {
       const gradient = context.createRadialGradient(
          this.state.width / 2,
          this.state.height - 300,
@@ -30,44 +36,21 @@ class Canvas extends Component {
       gradient.addColorStop(0.39, 'rgba(184, 115, 187, 0.75)');
       gradient.addColorStop(0.57, 'rgba(75, 73, 156, 0.75)');
       gradient.addColorStop(0.7, 'rgba(38, 38, 80, 0.75)');
+
+      return gradient;
+   }
+
+   drawCanvas() {
+      const canvas = this.canvasRef.current;
+      canvas.width = this.state.width;
+      canvas.height = this.state.height;
+      const context = canvas.getContext('2d');
+      const gradient = this.drawGradient(context);
       context.fillStyle = gradient;
       context.fillRect(0, 0, this.state.width, this.state.height);
       console.log('height:', this.state.height);
       console.log('width:', this.state.width);
    }
-
-   // drawGradient(ctx) {
-   //    const gradient = ctx.createRadialGradient(1000, 700, 200, 500, 500, 2000);
-   //    gradient.addColorStop(0, 'rgba(254, 245, 187, 0.5)');
-   //    gradient.addColorStop(0.13, 'rgba(252, 215, 140, 0.5)');
-   //    gradient.addColorStop(0.25, 'rgba(248, 165, 123, 0.5)');
-   //    gradient.addColorStop(0.39, 'rgba(184, 115, 187, 0.5)');
-   //    gradient.addColorStop(0.57, 'rgba(75, 73, 156, 0.5)');
-   //    gradient.addColorStop(0.7, 'rgba(38, 38, 80, 0.5)');
-
-   //    return gradient;
-
-   // drawCanvas() {
-
-   //    const canvas = this.canvasRef.current;
-   //    const context = canvas.getContext('2d');
-   //    const gradient = context.createRadialGradient(
-   //       50,
-   //       100,
-   //       100,
-   //       100,
-   //       200,
-   //       200
-   //    );
-   //    gradient.addColorStop(0, 'rgba(254, 245, 187, 0.5)');
-   //    gradient.addColorStop(0.13, 'rgba(252, 215, 140, 0.5)');
-   //    gradient.addColorStop(0.25, 'rgba(248, 165, 123, 0.5)');
-   //    gradient.addColorStop(0.39, 'rgba(184, 115, 187, 0.5)');
-   //    gradient.addColorStop(0.57, 'rgba(75, 73, 156, 0.5)');
-   //    gradient.addColorStop(0.7, 'rgba(38, 38, 80, 0.5)');
-   //    context.fillStyle = gradient;
-   //    context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-   // }
 
    render() {
       return (
