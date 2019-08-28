001cd0018197482d9887602e4889335262e19772
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-08-28 15:14:38 -0700

    Add basic page structure and react router.

--- client/src/Containers/Canvas/Canvas.js
+++ client/src/Containers/Canvas/Canvas.js
@@ -30,12 +30,12 @@ class Canvas extends Component {
          this.state.height / 2,
          this.state.height * 1.25
       );
-      gradient.addColorStop(0, 'rgba(254, 245, 187, 0.75)');
-      gradient.addColorStop(0.13, 'rgba(252, 215, 140, 0.75)');
-      gradient.addColorStop(0.25, 'rgba(248, 165, 123, 0.75)');
-      gradient.addColorStop(0.39, 'rgba(184, 115, 187, 0.75)');
-      gradient.addColorStop(0.57, 'rgba(75, 73, 156, 0.75)');
-      gradient.addColorStop(0.7, 'rgba(38, 38, 80, 0.75)');
+      gradient.addColorStop(0, 'rgba(254, 245, 187, 1)');
+      gradient.addColorStop(0.13, 'rgba(252, 215, 140, 1)');
+      gradient.addColorStop(0.25, 'rgba(248, 165, 123, 1)');
+      gradient.addColorStop(0.39, 'rgba(184, 115, 187, 1)');
+      gradient.addColorStop(0.57, 'rgba(75, 73, 156, 1)');
+      gradient.addColorStop(0.7, 'rgba(38, 38, 80, 1)');
 
       return gradient;
    }
