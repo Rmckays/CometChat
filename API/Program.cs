1ae0616cff821eb34e0144447fa01798e662a3e3
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-09-03 08:59:15 -0700

    Add db connection and create user table

--- API/Program.cs
+++ API/Program.cs
@@ -5,20 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         CreateWebHostBuilder(args).Build().Run();
+      }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+      public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+          WebHost.CreateDefaultBuilder(args)
+              .UseStartup<Startup>();
+   }
 }
