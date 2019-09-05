b57daecfe66161fc44e2ae4472db5c52164565bb
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-09-05 10:28:14 -0700

    Add create new user handler. Implement route to create new user.

---
+++ Persistence/Migrations/20190905164531_ModMessageMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ModMessageMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Messages",
+                newName: "CreatedAt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Messages",
+                newName: "Date");
+        }
+    }
+}
