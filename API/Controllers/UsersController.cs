b2e61ae671763eff116e04c5b5766e83e5fd13f0
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-09-05 11:28:56 -0700

    Add edit user route and handler.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -37,5 +37,12 @@ namespace API.Controllers
       {
          return await _mediator.Send(command);
       }
+
+      [HttpPut("{id}")]
+      public async Task<ActionResult<Unit>> Edit(Guid id, EditUser.Command command)
+      {
+         command.Id = id;
+         return await _mediator.Send(command);
+      }
    }
 }
