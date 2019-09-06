e2074c9693ab9b6ad32b603ad2583a9c671a3337
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-09-05 19:04:46 -0700

    Add delete handler and route.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -44,5 +44,11 @@ namespace API.Controllers
          command.Id = id;
          return await _mediator.Send(command);
       }
+
+      [HttpDelete("{id}")]
+      public async Task<ActionResult<Unit>> Delete(Guid id)
+      {
+         return await _mediator.Send(new DeleteUser.Command { Id = id });
+      }
    }
 }
--- Application/Users/DeleteUser.cs
+++ Application/Users/DeleteUser.cs
@@ -1,7 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Persistence;
+
 namespace Application.Users
 {
-    public class DeleteUser
-    {
-        
-    }
+   public class DeleteUser
+   {
+      public class Command : IRequest
+      {
+         public Guid Id { get; set; }
+         public string Username { get; set; }
+      }
+
+      public class Handler : IRequestHandler<Command>
+      {
+         private readonly ChatAppContext _context;
+         public Handler(ChatAppContext context)
+         {
+            _context = context;
+         }
+
+         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+         {
+            var user = await _context.Users.FindAsync(request.Id);
+
+            if (user == null)
+            {
+               throw new Exception("Cannot find user");
+            }
+
+            _context.Remove(user);
+
+            var success = await _context.SaveChangesAsync() > 0;
+
+            if (success) return Unit.Value;
+
+            throw new Exception("Problem saving changes");
+         }
+      }
+   }
 }
