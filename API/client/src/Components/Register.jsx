4b44f3346385ca4018c5a6af21acd2a2c3fc9d8f
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-09-12 07:13:22 -0700

    Destructure hooks for registerForm component.

--- API/client/src/Components/Register.jsx
+++ API/client/src/Components/Register.jsx
@@ -5,50 +5,42 @@ import axios from 'axios';
 
 const registerForm = () => {
 
-   const inputState = useState({id: '', username: '', name: '', email: '', password: ''})
+   const [usernameState, setUsernameState] = useState('');
+   const [nameState, setNameState] = useState('');
+   const [emailState, setEmailState] = useState('');
+   const [passwordState, setPasswordState] = useState('');
+   const [userIdState, setUserIdState] = useState('');
 
    const handleOnSubmit = () => {}
 
    const handleOnInputChange = event => {
-      const inputTarget = event.target.value;
-      inputState[1](prevInputState => ({
-         switch (inputName){
-            case 'username':
-               break;
-            case 'name':
-               break;
-            case 'email':
-               break;
-            case 'password':
-               break;
-         }
-      }));
+      
    }
 
    return (
       <Segment style={{margin: "15rem auto", background: "rgba(0,0,0,0)", width: "25%"}}>
          <Form onSubmit={handleOnSubmit}>
             <Form.Input 
-               onChange={event => inputState[1]({username: event.target.value})}
+               onChange={event => setUsernameState(event.target.value)}
                placeholder='Username' 
                name="username" 
-               value={inputState[0].username}/>
+               value={usernameState}/>
             <Form.Input 
-               onChange={event => inputState[1]({name: event.target.value})}
+               onChange={event => setNameState(event.target.value)}
                placeholder='Name' 
                name="name" 
-               value={inputState[0].name} />
+               value={nameState} />
             <Form.Input 
-               onChange={event => inputState[1]({email: event.target.value})}
+               onChange={event => setEmailState(event.target.value)}
                type='email' 
                placeholder='Email' 
                name="email" 
-               value={inputState[0].email}/>
+               value={emailState}/>
             <Form.Input 
-               onChange={event => inputState[1]({password: event.target.value})}
+               onChange={event => setPasswordState(event.target.value)}
                placeholder='Password' 
                name="password" 
-               value={inputState[0].password} />
+               value={passwordState} />
             <Button name="title" type='submit' content='Create' style={{width: "100%", background: "rgb(240, 156, 96)"}}/>
          </Form>
       </Segment>
