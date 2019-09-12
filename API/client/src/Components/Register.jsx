006a6a021274b19c4d0f65379c0dc08f9e62e227
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-09-11 20:54:09 -0700

    Add handleOnInputChange and handleOnSubmit methods for register component.

--- API/client/src/Components/Register.jsx
+++ API/client/src/Components/Register.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
+import {v4 as uuid} from "uuid";
+import axios from 'axios';
 
 const registerForm = () => {
+
+   const handleOnSubmit = () => {}
+
+   const handleOnInputChange = () => {}
+
    return (
       <Segment style={{margin: "15rem auto", background: "rgba(0,0,0,0)", width: "25%"}}>
-         <Form>
+         <Form onSubmit={handleOnSubmit}>
             <Form.Input 
+               onChange={handleOnInputChange}
                placeholder='Username' 
                name="username" 
                value=""/>
             <Form.Input 
+               onChange={handleOnInputChange}
                placeholder='Name' 
                name="name" 
                value="" />
             <Form.Input 
+               onChange={handleOnInputChange}
                type='email' 
                placeholder='Email' 
                name="email" 
                value=""/>
             <Form.Input 
+               onChange={handleOnInputChange}
                placeholder='Password' 
                name="password" 
                value="" />
