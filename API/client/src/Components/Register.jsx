a739d5dece0df525425e1364f5861cf30d44563d
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-09-11 21:38:02 -0700

    Add useState hook to register component.

--- API/client/src/Components/Register.jsx
+++ API/client/src/Components/Register.jsx
@@ -5,34 +5,50 @@ import axios from 'axios';
 
 const registerForm = () => {
 
+   const inputState = useState({id: '', username: '', name: '', email: '', password: ''})
+
    const handleOnSubmit = () => {}
 
-   const handleOnInputChange = () => {}
+   const handleOnInputChange = event => {
+      const inputTarget = event.target.value;
+      inputState[1](prevInputState => ({
+         switch (inputName){
+            case 'username':
+               break;
+            case 'name':
+               break;
+            case 'email':
+               break;
+            case 'password':
+               break;
+         }
+      }));
+   }
 
    return (
       <Segment style={{margin: "15rem auto", background: "rgba(0,0,0,0)", width: "25%"}}>
          <Form onSubmit={handleOnSubmit}>
             <Form.Input 
-               onChange={handleOnInputChange}
+               onChange={event => inputState[1]({username: event.target.value})}
                placeholder='Username' 
                name="username" 
-               value=""/>
+               value={inputState[0].username}/>
             <Form.Input 
-               onChange={handleOnInputChange}
+               onChange={event => inputState[1]({name: event.target.value})}
                placeholder='Name' 
                name="name" 
-               value="" />
+               value={inputState[0].name} />
             <Form.Input 
-               onChange={handleOnInputChange}
+               onChange={event => inputState[1]({email: event.target.value})}
                type='email' 
                placeholder='Email' 
                name="email" 
-               value=""/>
+               value={inputState[0].email}/>
             <Form.Input 
-               onChange={handleOnInputChange}
+               onChange={event => inputState[1]({password: event.target.value})}
                placeholder='Password' 
                name="password" 
-               value="" />
+               value={inputState[0].password} />
             <Button name="title" type='submit' content='Create' style={{width: "100%", background: "rgb(240, 156, 96)"}}/>
          </Form>
       </Segment>
