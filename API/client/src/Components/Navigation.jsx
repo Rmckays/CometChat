ff79e55ea2681e80deb598699cd444da7e59e754
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-09-29 12:18:55 -0700

    Add logo to navigation.

--- API/client/src/Components/Navigation.jsx
+++ API/client/src/Components/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from "../cometchatlogo.png";
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
    return (
       <div className='m-1 p-1'>
          <nav className='d-flex text-white justify-content-between align-content-center m-1 p-1'>
-            <div className='p-1 m-1 brand'>CometChat</div>
+            <div className='p-1 m-1 brand'><a href="/"><img alt="CometChat" src={logo}/></a></div>
             <div className='m-1 p-1'>
                <ul className='d-flex nav-list py-1 mx-2'>
                   <li className='px-3'>
